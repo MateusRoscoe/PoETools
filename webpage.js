b3c26eba193ca2fc8b1aa165e5a6e9c4e287fac6
Mateus <mateusroscoe2@gmail.com>
2020-07-20 19:21:03 -0300

    Login check implemented in master branch

--- webpage.js
+++ webpage.js
@@ -1,9 +1,7 @@
 const puppeteer = require('puppeteer')
-const cheerio = require('cheerio')
 class Webpage {
     static async updateFilter() {
         var url = 'https://www.filterblade.xyz'
-        var hasLogin = true // toDO implement a method to detect if login data is present or not.
 
         var saveSlotFB = '1' // valid numbers are 0~14
         var saveSlotPoE = '2' // valid numbers are 0~14?
@@ -15,41 +13,54 @@ class Webpage {
         });
         const page = await browser.newPage()
         await page.goto(url)
-        if (hasLogin == true) {
-            try {
-                // Navigating to Load Filter section
-                await page.waitForSelector('button[id="SelectionButton5"]')
-                await new Promise(r => setTimeout(r, 2000))
-                await page.click('button[id="SelectionButton5"]')
-                // Clicking to load desired filter.
-                let nextButtonId = 'LoadProfileSaveState' + saveSlotFB
-                await page.waitForSelector('button[id="'+nextButtonId+'"]')
-                await page.click('button[id="' + nextButtonId + '"]')
-                // Waiting for filter to load and possibly update
-                await page.waitForSelector('div.smallMessageBox')
-                console.log('Save state successfully loaded.')
+        try {
+            // Waiting for page to load properly
+            await page.waitForSelector('button[id="SelectionButton5"]')
+            await new Promise(r => setTimeout(r, 2000))
+            // Checking if is logged in
+            await page.waitForSelector('div[id=loginSessionInfo]', {
+                visible: true,
+                timeout: 5000,
+            })
+            // Navigating to Load Filter section
+            await page.click('button[id="SelectionButton5"]')
+            // Clicking to load desired filter.
+            let nextButtonId = 'LoadProfileSaveState' + saveSlotFB
+            await page.waitForSelector('button[id="' + nextButtonId + '"]')
+            await page.click('button[id="' + nextButtonId + '"]')
+            // Waiting for filter to load and possibly update
+            await page.waitForSelector('div.smallMessageBox')
+            console.log('Save state successfully loaded.')
 
-                // Syncing with PoE
-                await page.click('a[href = "#"]')
-                await page.click('button[id="uploadFilterToPoeButton"]')
-                await page.waitForSelector('button[id="UpdatePoeFilterContent' + saveSlotPoE + '"]')
-                await page.click('button[id="UpdatePoeFilterContent'+ saveSlotPoE +'"]')
+            // Syncing with PoE
+            await page.click('a[href = "#"]')
+            await page.click('button[id="uploadFilterToPoeButton"]')
+            await page.waitForSelector('button[id="UpdatePoeFilterContent' + saveSlotPoE + '"]')
+            await page.click('button[id="UpdatePoeFilterContent' + saveSlotPoE + '"]')
 
-                // toDO check if it's possible that the update fails and an error message is displayed as a label
-                // Wait for success message + 2s then close the browser
-                await page.waitForSelector('div.smallMessageBox')
-                await new Promise(r => setTimeout(r, 2000))
-                console.log("Job finished, filter updated.")
-                browser.close()
+            // toDO check if it's possible that the update fails and an error message is displayed as a label
+            // Wait for success message + 2s then close the browser
+            await page.waitForSelector('div.smallMessageBox')
+            await new Promise(r => setTimeout(r, 10000))
+            console.log("Job finished, filter updated.")
+            browser.close()
+        }
+        // Handling possible errors
+        catch (err) {
+            // Not logged in
+            if (err.message.includes('waiting for selector "div[id=loginSessionInfo]" failed')) {
+                console.log("User not logged in to filterblade.xyz");
+                this.doLogin()
             }
-            catch (err) {
+            // Don't know which error happened, just log and move on
+            else {
                 console.log(err)
-                //console.log("Não foi possível buscar os dados desejados.")
             }
         }
-        else{ // do login
-            console.error("Not logged in.");
-        }
+    }
+
+    static doLogin() { // Start a new session with a visible browser that the user can log in and save the data
+        console.log("login method not implemented yet");
     }
 }
 
